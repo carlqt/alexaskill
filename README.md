a618050ac2f2308b1c4fbad707a92c05e2e3fd6a
Carl Tablante <carl@guavapass.com>
2017-07-01 20:48:07 +0800

    Added examples in README and simple refactor

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 ## Install
-go get -u github.com/carlqt/alexa_skill
+go get -u github.com/carlqt/alexaskill
+
+## Middleware
+The library comes with a middleware to handle amazons validation of Signature. Just wrap `AlexaValidation`  middleware to any of your handlers.
+
+## Example
+```go
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/carlqt/alexaskill"
+	"github.com/carlqt/alexaskill/middleware"
+	"github.com/carlqt/alexaskill/response"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	var appID := "amazon.app.id.kdjakjf"
+	r := mux.NewRouter()
+	alexaHandler := http.HandlerFunc(index)
+	r.Handle("/", middleware.AlexaValidation(alexaHandler)).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	advice := "Carl says: Star this repository now"
+
+	alexaReq, err := alexaskill.AlexaNewRequest(r.Body)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if alexaReq.AppID() != appID {
+		w.WriteHeader(404)
+		return
+	}
+
+	if alexaReq.Type() == "IntentRequest" {
+		switch alexaReq.IntentName() {
+		case "AMAZON.CancelIntent":
+			response.AlexaText("Cancelled").SimpleCard("Cancel", "cancel").Respond(w, 200)
+		case "RandomAdvice":
+			response.AlexaText(advice).SimpleCard("Advice", advice).Respond(w, 200)
+		default:
+			log.Fatal("unrecognized")
+		}
+	}
+}
+
+
+```
--- alexaskill.go
+++ alexaskill.go
@@ -1,13 +1,89 @@
+// A typical incoming alexa request looks like this.
+// {
+//   "session": {
+//     "sessionId": "SessionId.ec3aa84e-c489-4f0a-ba50-8b67d9673452",
+//     "application": {
+//       "applicationId": "amzn1.ask.skill.3aebac54-38a0-4dd3-9f17-4942972e4136"
+//     },
+//     "attributes": {},
+//     "user": {
+//       "userId": "amzn1.ask.account.AFWSUCAS2MHVONEYZ6YDMODQ4FDXFJTHOATXZZ2XODC7L5SECMNIXBDUIP3WKGDPCWE26LXCPOB6EII3H46CSPKJGLOIB3CLZTTNNJHFGFQEG73YBO65UPFNLXVKJXZ3RTT4EOMSU2IJWOQ6JDQYNNYYWAZX6E46ERLJKCTGGZQEFBU6XM5ZBS7YL2G2PEEVI6VOJ4P5WAYZJGY"
+//     },
+//     "new": false
+//   },
+//   "request": {
+//     "type": "IntentRequest",
+//     "requestId": "EdwRequestId.c30686c6-3e00-4da6-b49b-e1a49e2ade27",
+//     "locale": "en-US",
+//     "timestamp": "2017-07-02T02:10:24Z",
+//     "intent": {
+//       "name": "AMAZON.CancelIntent",
+//       "slots": {}
+//     }
+//   },
+//   "version": "1.0"
+// }
+
 package alexaskill
 
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+)
+
+// AlexaRequest is the struct of the incoming alexa request
 type AlexaRequest struct {
 	Request struct {
 		Type      string `json:"type"`
 		RequestID string `json:"requestId"`
-		Intent    Intent `intent`
+		Intent    Intent `json:"intent"`
 	} `json:"request"`
+
+	Session Session `json:"session"`
 }
 
+// Session object of alexa request
+type Session struct {
+	New         bool
+	SessionID   string `json:"sessionId"`
+	Application struct {
+		ApplicationID string `json:"applicationId"`
+	} `json:"application"`
+}
+
+// Intent object of alexa request
 type Intent struct {
 	Name string `json:"name"`
+}
+
+// AlexaNewRequest is a constructor that reads the request.Body from Amazon
+func AlexaNewRequest(body io.ReadCloser) (*AlexaRequest, error) {
+	rBody, err := ioutil.ReadAll(body)
+	if err != nil {
+		return nil, err
+	}
+
+	alexa := new(AlexaRequest)
+	err = json.Unmarshal(rBody, alexa)
+	if err != nil {
+		return nil, err
+	}
+
+	return alexa, nil
+}
+
+//Type is a function shortcut to get AlexaRequest.Request.Type
+func (a *AlexaRequest) Type() string {
+	return a.Request.Type
+}
+
+//IntentName is a function shortcut to get AlexaRequest.Request.Intent.Name
+func (a *AlexaRequest) IntentName() string {
+	return a.Request.Intent.Name
+}
+
+//AppID is a function shortcut to get AlexaRequest.Request.Intent.Name
+func (a *AlexaRequest) AppID() string {
+	return a.Session.Application.ApplicationID
 }
